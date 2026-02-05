a77ddf061141af94a1f77a1cd4d3ddf2d231fb0b
Bruce Rodriguez Ebreo <bebreo2027@cchsdons.com>
2026-02-05 08:50:26 -0800

    add special names for certain card numbers like Ace and King

--- TwoDArrays.java
+++ TwoDArrays.java
@@ -14,6 +14,8 @@ public class TwoDArrays {
         //2d array with 4 suits and 13 numbers
         String[][] deck = new String[4][13];
         String suitName = "";
+        //if the number has a special name, like Ace or King
+        String numTitle = "";
         Random r = new Random();
         // load the 2d array with the cards and name the suits accordingly
         for (int suit=0; suit < deck.length; suit++) {
@@ -30,9 +32,27 @@ public class TwoDArrays {
                 case 3:
                     suitName = "Diamonds";
                     break;
+                default:
+                    throw new AssertionError();
             }
-            for (int num=0; num < deck[suit].length; num++) {
-                deck[suit][num]= (num + 1) + " of " + suitName;
+            for (int l=0; l < deck[suit].length; l++) {
+                switch (l) {
+                    case 0:
+                        numTitle = "Ace";
+                        break;
+                    case 10:
+                        numTitle = "Jack";
+                        break;
+                    case 11:
+                        numTitle = "Queen";
+                        break;
+                    case 12:
+                        numTitle = "King";
+                        break;
+                    default:
+                        numTitle = numTitle.valueOf(l + 1);
+                }
+                deck[suit][l]= numTitle + " of " + suitName;
             }
         }
         System.out.println("4 hands of 5 random cards: \n");
